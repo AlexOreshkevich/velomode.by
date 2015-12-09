f4a878e5a9a958c88944c18159e48f3c8c8ad27e
vasil.malakhouski <vasil.malakhouski@redsoft.pro>
2015-12-09 11:10:02 +0300

    first commit. part 2: non folder files

---
+++ new.php
@@ -0,0 +1,12 @@
+// verify that this is a product category page
+if (is_product_category()){
+    global $wp_query;
+    // get the query object
+    $cat = $wp_query->get_queried_object();
+    // get the thumbnail id user the term_id
+    $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true ); 
+    // get the image URL
+    $image = wp_get_attachment_url( $thumbnail_id ); 
+    // print the IMG HTML
+    echo '<img src="'.$image.'" alt="" width="762" height="365" />';
+}
